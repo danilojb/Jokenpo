7dc0bed028d03aac42fd5b7067d439a4b81bd55f
Danilo José <danilojb@gmail.com>
2022-08-15 01:24:40 +0000

    New get_choices funtion

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+def get_choices():
+  player_choice = input("Enter a choice (rock, paper, scissors: )")
+  computer_options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(computer_options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  #return player_choice
+  return choices
+
+choices = get_choices()
+print(choices)
+
